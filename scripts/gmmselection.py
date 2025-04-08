0cb4a6a4c16448989e9547fe891675b0d0cb2e8c
Emily Weiss <emily.weiss@arcadiascience.com>
2025-04-08 16:19:50 -0700

    linting
    Signed-off-by: Emily Weiss <emily.weiss@arcadiascience.com>

--- scripts/gmmselection.py
+++ scripts/gmmselection.py
@@ -1,10 +1,12 @@
 import argparse
+import numpy
 import os
+import pandas
 import re
-import numpy as np
-import pandas as pd
+
 from sklearn.mixture import BayesianGaussianMixture
 from sklearn.preprocessing import StandardScaler
+
 
 def find_and_process_tsv_files(root_directory, verbose=True):
     """
